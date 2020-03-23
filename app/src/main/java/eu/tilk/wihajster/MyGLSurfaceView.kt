d48ab52bda0b48cea0a10b03897c75ce6948f2bf
Marek Materzok <tilk@tilk.eu>
2020-03-23 20:36:11 +0100

    Drawing empty string notes

--- app/src/main/java/eu/tilk/wihajster/MyGLSurfaceView.kt
+++ app/src/main/java/eu/tilk/wihajster/MyGLSurfaceView.kt
@@ -256,6 +256,66 @@ class Note : Shape(vertexCoords, drawOrder, mProgram) {
     }
 }
 
+class EmptyStringNote : Shape(vertexCoords, drawOrder, mProgram) {
+    companion object {
+        private val vertexCoords = floatArrayOf(
+            0f, -0.12f, 0.0f,
+            0f, 0.12f, 0.0f,
+            1f, 0.12f, 0.0f,
+            1f, -0.12f, 0.0f
+        )
+        private val drawOrder = shortArrayOf(
+            0, 1, 2, 0, 2, 3
+        )
+        private val vertexShaderCode = """
+            #version 300 es
+            uniform mat4 uMVPMatrix;
+            uniform vec4 uPosition;
+            uniform int uWidth;
+            in vec4 vPosition;
+            out vec2 vTexCoord;
+            void main() {
+                vec4 rPosition = vec4(vPosition.x * float(uWidth), vPosition.y, vPosition.z, vPosition.w); 
+                gl_Position = uMVPMatrix * (rPosition + uPosition);
+                vTexCoord = vec2((vPosition.x - 0.5) / 0.5, vPosition.y / 0.12);
+            }
+        """.trimIndent()
+        private val fragmentShaderCode = """
+            #version 300 es
+            precision mediump float;
+            uniform int uString;
+            in vec2 vTexCoord;
+            out vec4 FragColor;
+            $stringColorsGLSL
+            void main() {
+                float scl = (tanh(-abs(vTexCoord.y*10.0)+3.0) + 1.0) / 2.0;
+                FragColor = vec4(stringColors[uString], scl);
+            }
+        """.trimIndent()
+        private var mProgram : Int = -1
+        fun initialize() {
+            val vertexShader = loadShader(GL_VERTEX_SHADER, vertexShaderCode)
+            val fragmentShader = loadShader(GL_FRAGMENT_SHADER, fragmentShaderCode)
+            mProgram = makeProgramFromShaders(vertexShader, fragmentShader)
+        }
+    }
+    fun draw(mvpMatrix : FloatArray, time : Float, note : Event.Note,
+             fret : Int, width : Int, scrollSpeed : Float) {
+        prepare(mvpMatrix)
+        val positionHandle = glGetUniformLocation(mProgram, "uPosition")
+        glUniform4f(positionHandle,
+            fret - 1f,
+            1.5f * (note.string + 0.5f) / 6f,
+            (time - note.time) * scrollSpeed,
+            0f)
+        val fretHandle = glGetUniformLocation(mProgram, "uWidth")
+        glUniform1i(fretHandle, width)
+        val stringHandle = glGetUniformLocation(mProgram, "uString")
+        glUniform1i(stringHandle, note.string.toInt())
+        internalDraw()
+    }
+}
+
 class Neck : StaticShape(vertexCoords, drawOrder, mProgram) {
     companion object {
         private val vertexCoords = floatArrayOf(
@@ -446,6 +506,7 @@ class MyGLRenderer(val song : List<Event>, private val context : Context) : GLSu
     private lateinit var textures : Textures
     private lateinit var fretNumbers : FretNumbers
     private lateinit var note : Note
+    private lateinit var emptyStringNote : EmptyStringNote
     private lateinit var beat : Beat
     private val vPMatrix = FloatArray(16)
     private val projectionMatrix = FloatArray(16)
@@ -470,12 +531,14 @@ class MyGLRenderer(val song : List<Event>, private val context : Context) : GLSu
         FretNumbers.initialize()
         Tab.initialize()
         Note.initialize()
+        EmptyStringNote.initialize()
         Beat.initialize()
         textures = Textures(context)
         neck = Neck()
         fretNumbers = FretNumbers(textures)
         tab = Tab()
         note = Note()
+        emptyStringNote = EmptyStringNote()
         beat = Beat()
         lastFrameTime = SystemClock.elapsedRealtime()
     }
@@ -526,10 +589,17 @@ class MyGLRenderer(val song : List<Event>, private val context : Context) : GLSu
         for (evt in scroller.activeEvents.reversed()) {
             while (anchors[anchorsIdx].time > evt.time) anchorsIdx++
             when (evt) {
-                is Event.Note -> note.draw(vPMatrix, scroller.currentTime, evt, scrollSpeed)
-                is Event.Beat -> beat.draw(vPMatrix, scroller.currentTime, evt,
-                    anchors[anchorsIdx].fret.toInt(), anchors[anchorsIdx].width.toInt(),
-                    scrollSpeed)
+                is Event.Note ->
+                    if (evt.fret > 0)
+                        note.draw(vPMatrix, scroller.currentTime, evt, scrollSpeed)
+                    else
+                        emptyStringNote.draw(vPMatrix, scroller.currentTime, evt,
+                            anchors[anchorsIdx].fret.toInt(), anchors[anchorsIdx].width.toInt(),
+                        scrollSpeed)
+                is Event.Beat ->
+                    beat.draw(vPMatrix, scroller.currentTime, evt,
+                        anchors[anchorsIdx].fret.toInt(), anchors[anchorsIdx].width.toInt(),
+                        scrollSpeed)
             }
         }
 
